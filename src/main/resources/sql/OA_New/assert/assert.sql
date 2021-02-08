dea59b92507c9f934ccb8cffe77114255938055f
qk l <1733856225@qq.com>
2021-02-08 14:52:59 +0800

    资产表修改

--- src/main/resources/sql/OA_New/assert/assert.sql
+++ src/main/resources/sql/OA_New/assert/assert.sql
@@ -1,7 +1,7 @@
 -- 资产表
 -- 资产维护表
-drop table if exists t_asset_defend;
-CREATE TABLE t_asset_defend(
+drop table if exists t_ass_defend;
+CREATE TABLE t_ass_defend(
 asset_no		 varchar(22)  	NOT NULL	COMMENT '资产编号',
 asset_nature		 varchar(1)	NOT NULL	COMMENT '资产性质',
 asset_sort		varchar(2)		COMMENT  '资产种类',
@@ -13,6 +13,10 @@ asset_team		 varchar(10)		COMMENT '资产所属团队',
 asset_project		 varchar(10)		COMMENT '资产所属项目组',
 asset_upper_code		 varchar(22)		COMMENT '父级资产编号',
 asset_buy_date		datetime  		COMMENT '购买日期',
+
+asset_current_status_date		datetime  COMMENT '当前状态日期',
+asset_attribute_level  varchar(1) COMMENT'使用类型',
+
 asset_warranty_period		bigint(4)		COMMENT '有效期限',
 asset_use_num		 varchar(1)		COMMENT '使用次数',
 asset_back_info		 varchar(255)		COMMENT '信息备注',
@@ -22,8 +26,8 @@ PRIMARY KEY (asset_no)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8  COMMENT '资产维护表';
 
 -- 资产人员关联表
-drop table if exists t_asset_person;
-CREATE TABLE t_asset_person(
+drop table if exists t_ass_person;
+CREATE TABLE t_ass_person(
     asset_no	 varchar(22)  	NOT NULL	COMMENT'资产编号',
     asset_staff_code	 varchar(10)	  	NOT NULL	COMMENT'资产使用员工工号',
     insert_time_for_his	 datetime	NULL  DEFAULT NOW()	COMMENT'插入时间',
@@ -32,8 +36,8 @@ CREATE TABLE t_asset_person(
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '资产人员关联表';
 
 -- 资产轨迹表
-drop table if exists t_asset_track;
-CREATE TABLE t_asset_track(
+drop table if exists t_ass_track;
+CREATE TABLE t_ass_track(
     asset_id     int            NOT NULL	COMMENT'资产轨迹id',
     asset_no	 varchar(22)  	NOT NULL	COMMENT'资产编号',
     asset_staff_code	 varchar(10)	NOT NULL	COMMENT'资产使用员工工号',
@@ -45,8 +49,8 @@ CREATE TABLE t_asset_track(
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '资产轨迹表';
 
 -- 资产申请表
-drop table if exists t_asset_apply;
-CREATE TABLE  t_asset_apply(
+drop table if exists t_ass_apply;
+CREATE TABLE  t_ass_apply(
     asset_apply_no	 varchar(22)  	NOT NULL	COMMENT'申请任务编号',
     asset_apply_type	 varchar(1)		COMMENT'申请类型',
     asset_apply_reason	 varchar(1)		COMMENT'申请原因',
@@ -66,12 +70,15 @@ CREATE TABLE  t_asset_apply(
 
 
 -- 资产申请清单表
-drop table if exists t_asset_apply_list;
-CREATE TABLE  t_asset_apply_list(
+drop table if exists t_ass_apply_list;
+CREATE TABLE  t_ass_apply_list(
     asset_apply_no	 varchar(22)  	NOT NULL	COMMENT'申请任务编号',
     asset_apply_staff_code	 varchar(10)	NOT NULL	COMMENT'资产使用员工工号',
     asset_nature	 varchar(1)		COMMENT'资产性质',
     asset_sort	varchar(2)		COMMENT'资产种类',
+
+    asset_attribute_level  varchar(1) COMMENT'使用类型',
+
     asset_quantity	bigint(4)		COMMENT'资产数量',
     asset_config	 varchar(255)		COMMENT'资产配置',
     asset_buy_way	 varchar(255)		COMMENT'资产采买途径',
@@ -86,29 +93,31 @@ CREATE TABLE  t_asset_apply_list(
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '资产申请清单表';
 
 -- 审批规则配置表
-drop table if exists t_asset_appr_rule_config;
-CREATE TABLE  t_asset_appr_rule_config(
-    asset_check_type	 varchar(1)  	NOT NULL	COMMENT'审核类型',
+drop table if exists t_ass_appr_rule_config;
+CREATE TABLE  t_ass_appr_rule_config(
+    asset_check_type	 varchar(2)  	NOT NULL	COMMENT'审核类型',
     asset_check_person_code	 varchar(10)	NOT NULL	COMMENT'审核人员代码',
     asset_check_person_name	 varchar(30)		COMMENT'审核人员名称',
 
+    asset_check_object	 varchar(1)		COMMENT'审核对象',
     asset_nature	 varchar(1)		COMMENT'资产性质',
 
     asset_min_amount	 decimal(8,2)		COMMENT'最小金额',
     asset_max_amount	 decimal(8,2)		COMMENT'最大金额',
     asset_check_rule	 varchar(1)  		COMMENT'审核规则',
 
+    asset_defend_date	 datetime  		COMMENT'维护日期',
     asset_defend_code	 varchar(10)  		COMMENT'维护人员',
 
     is_validate_flag	 varchar(1)  		COMMENT'审核是否有效标志',
     insert_time_for_his	 datetime	NULL  DEFAULT NOW()	COMMENT'插入时间',
     operate_time_for_his	datetime	NULL  DEFAULT NOW()	COMMENT'更新时间',
-    PRIMARY KEY (asset_check_type,asset_check_person_code)
+    PRIMARY KEY (asset_check_type,asset_check_person_code,asset_check_object)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '审批规则配置表';
 
 -- 标准字段表
-drop table if exists t_asset_stand_field;
-CREATE TABLE  t_asset_stand_field(
+drop table if exists t_ass_stand_field;
+CREATE TABLE  t_ass_stand_field(
     asset_sort	varchar(2)	NOT NULL	COMMENT'业务类型',
     asset_no	varchar(22)	NOT NULL	COMMENT'字段代码',
     asset_no_name	varchar(255)		COMMENT'字段名称',
@@ -116,6 +125,7 @@ CREATE TABLE  t_asset_stand_field(
     is_validate_flag	varchar(1)		COMMENT'是否有效',
 
     asset_defend_code	varchar(10)		COMMENT'维护人员',
+    asset_defend_date	datetime		COMMENT'维护日期',
 
     expand_field_one	varchar(255)		COMMENT'扩展字段1',
     expand_field_two	varchar(255)		COMMENT'扩展字段2',
